3f3788ffa928f555d7789e7274cb686e4115253b
llorencs <llorencs@gmail.com>
2017-01-20 00:36:25 +0100

    Add files via upload

--- CSV/csvFiles.py
+++ CSV/csvFiles.py
@@ -1,8 +1,10 @@
 import csv
-fileName=r'Samples\SampleCSVFile_11kb.csv'
+fileName = r'Samples\SampleCSVFile_11kb.csv'
 included_cols=[1, 8]
 with open(fileName, 'r', encoding='cp850') as fdata:
-    reader=csv.reader(fdata)
+    reader = csv.reader(fdata)
+    num_cols = len(next(reader)) #read first line and counts columns
+    print ('The number of columns is: ', num_cols)
     for row in reader:
         print(row[1]) 
         content = list(row[i] for i in included_cols)
